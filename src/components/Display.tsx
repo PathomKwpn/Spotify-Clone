7b9625618d21d701f11053eae0cc648f91c3f909
Pathompong <Pathompong.forwork@gmail.com>
2024-09-11 13:17:32 +0700

    [#11-09-2024] Add bg-Album Page follow bgColor

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,10 +1,36 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
 import DisplayAlbum from "./DisplayAlbum";
+import { useEffect, useRef } from "react";
+import { albumsData } from "../assets/assets";
 
 const Display = () => {
+  const displayRef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  console.log("isAlbum", isAlbum);
+
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  console.log(albumId);
+
+  const bgColor = albumsData[Number(albumId)]?.bgColor;
+  console.log(bgColor);
+  console.log("++++", displayRef.current);
+
+  useEffect(() => {
+    if (displayRef.current) {
+      if (isAlbum) {
+        displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
+      } else {
+        displayRef.current.style.background = "#121212";
+      }
+    }
+  });
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+    <div
+      ref={displayRef}
+      className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
+    >
       <Routes>
         <Route path="/" element={<DisplayHome />}></Route>
         <Route path="/album/:id" element={<DisplayAlbum />}></Route>
