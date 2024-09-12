1994a7aa1e1e1b6a07c4a9c4fb9f4d4deae270c6
Pathompong <Pathompong.forwork@gmail.com>
2024-09-12 16:50:07 +0700

    [#12-09-2024] Import react-icon & create search bar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { FaSearch } from "react-icons/fa";
+import { FaBoxArchive } from "react-icons/fa6";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -7,7 +9,7 @@ const Navbar = () => {
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 mr-8">
           <img
             onClick={() => navigate(-1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
@@ -21,6 +23,22 @@ const Navbar = () => {
             alt=""
           />
         </div>
+        <div className="relative flex flex-row items-center gap-4">
+          <form role="search" className="relative" action="">
+            <div className="h-[36px] absolute top-[10px] left-4 flex">
+              <FaSearch className="text-lg" />
+            </div>
+            <div className="">
+              <input
+                className="w-[24vw] h-[36px] bg-[#1f1f1f] rounded-full px-10"
+                type="text"
+              />
+            </div>
+            <div className="h-[36px] absolute top-[10px] right-4  flex flex-end">
+              <FaBoxArchive className="text-lg" />
+            </div>
+          </form>
+        </div>
         <div className="flex items-center gap-4">
           <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">
             Explore Premium
@@ -28,7 +46,7 @@ const Navbar = () => {
           <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer">
             Install App
           </p>
-          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">
+          <p className="bg-purple-500 text-black p-4 w-10 h-10 rounded-full flex items-center justify-center border-4 border-opacity-50">
             D
           </p>
         </div>
